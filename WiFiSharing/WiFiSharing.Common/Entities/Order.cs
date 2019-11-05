da0449d9d71b6a8bc527cac0a542684562d86051
Andrii Kozhevnikov <a.kozhevnikov@aimprosoft.com>
2019-11-05 19:49:30 +0200

    create entities and dbcontext

---
+++ WiFiSharing/WiFiSharing.Common/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WiFiSharing.Common.Enums;
+
+namespace WiFiSharing.Common.Entities
+{
+    public class Order
+    {
+        public User User { get; set; }
+        public int UserId { get; set; }
+        public Drone Drone { get; set; }
+        public int DroneId { get; set; }
+        public DateTime Date { get; set; }
+        public bool Relevance { get; set; }
+        public OrderType Type { get; set; }
+    }
+}
---
+++ WiFiSharing/WiFiSharing.DAL/AppDBContext.cs
@@ -0,0 +1,38 @@
+﻿namespace WiFiSharing.DAL
+{
+    using Microsoft.EntityFrameworkCore;
+    using WiFiSharing.Common.Entities;
+
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Drone> Drones { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>()
+                .HasKey(x => x.Id);
+
+            builder.Entity<Drone>()
+                .HasKey(x => x.Id);
+
+            builder.Entity<Order>()
+                .HasKey(x => new { x.DroneId, x.UserId });
+
+            builder.Entity<Order>()
+                .HasOne(x => x.Drone)
+                .WithMany(x => x.Orders)
+                .HasForeignKey(x => x.DroneId);
+
+            builder.Entity<Order>()
+                .HasOne(x => x.User)
+                .WithMany(x => x.Orders)
+                .HasForeignKey(x => x.UserId);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
