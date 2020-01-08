c210dd93184db18bed3065e50eebf6a52f5bc326
Andrey Kozhevnikov <andrii.kozhevnikov@nure.ua>
2020-01-09 00:22:33 +0200

    create layout for sign-in and sign-up

--- WiFiSharing/WiFiSharing.WEB/ClientApp/src/app/app.module.ts
+++ WiFiSharing/WiFiSharing.WEB/ClientApp/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignInComponent } from './components/authorization/sign-in/sign-in.component';
+import { SignUpComponent } from './components/authorization/sign-up/sign-up.component';
+
+import { NgxMaskModule, IConfig } from 'ngx-mask'
+
+const appRoutes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
