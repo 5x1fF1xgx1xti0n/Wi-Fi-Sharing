7e834d5f31f0818a17201761816adf02c4a4a790
Andrii Kozhevnikov <a.kozhevnikov@aimprosoft.com>
2020-01-09 17:41:57 +0200

    main layout and styling

--- WiFiSharing/WiFiSharing.WEB/ClientApp/src/app/app.module.ts
+++ WiFiSharing/WiFiSharing.WEB/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,19 +9,39 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { SignInComponent } from './components/authorization/sign-in/sign-in.component';
 import { SignUpComponent } from './components/authorization/sign-up/sign-up.component';
 
-import { NgxMaskModule, IConfig } from 'ngx-mask'
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { HomeComponent } from './components/home/home.component'
+import { MainComponent } from './components/main/main.component';
+import { DetailsInfoComponent } from './components/details-info/details-info.component';
 
 const appRoutes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'details-info',
+        component: DetailsInfoComponent
+      }
+    ]
+  },
   { path: 'sign-in', component: SignInComponent },
-  { path: 'sign-up', component: SignUpComponent}
+  { path: 'sign-up', component: SignUpComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    MainComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    HomeComponent,
+    DetailsInfoComponent
   ],
   imports: [
     BrowserModule,
